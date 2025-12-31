73034abc8b6e772af7a8786e6894e7b4bd81e541
cbecerra <cbecerra@ifp-alguero.org>
2025-12-31 13:53:17 +0100

    modificació  header

--- js/script.js
+++ js/script.js
@@ -2,3 +2,8 @@ function missatge() {
     const missatgeElement = document.getElementById('missatge');
     missatgeElement.style.display = 'block';
 }
+
+setTimeout(() => {
+    const textoLogo = document.getElementById('texto-logo');
+    textoLogo.style.display = 'block';
+}, 5000);
